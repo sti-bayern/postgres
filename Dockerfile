d836ab1e94bf218a6edd3a5d41eb468e880d269a
buffy <guenther.morhart@adbv-a.bayern.de>
2017-10-24 15:40:20 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM adbv/base
+
+LABEL maintainer="Günther Morhart"
+
+#
+# Environment variables
+#
+ENV PGDATA=/data \
+    PGPASS=app
+
+#
+# Setup
+#
+RUN apk add --no-cache \
+        postgresql \
+        postgresql-contrib && \
+    rm -rf \
+        /var/lib/postgresql \
+        /var/log/postgresql && \
+    mkdir /run/postgresql && \
+    chown -R app:app /run/postgresql
+
+#
+# Ports
+#
+EXPOSE 5432
+
+#
+# Command
+#
+COPY app-init.sh /usr/local/bin/app-init
+
+CMD ["su-exec", "app", "postgres"]
---
+++ app-init.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$(ls -A /data)" ]; then
+    su-exec app initdb -E UTF8 -U app
+
+    cat >> /data/postgresql.conf << EOF
+listen_addresses='*'
+log_directory = '/var/log/app'
+EOF
+
+    cat > /data/pg_hba.conf << EOF
+local   all             all                                     peer
+host    all             all             127.0.0.1/32            md5
+host    all             all             ::1/128                 md5
+host    all             all             172.16.0.0/12           md5
+host    all             all             10.0.0.0/8              md5
+EOF
+
+    cat << EOF | su-exec app postgres --single postgres
+CREATE DATABASE app ENCODING 'UTF8';
+ALTER USER app WITH PASSWORD '$PGPASS';
+EOF
+
+    if [ -f /app.sql ]; then
+        su-exec app pg_ctl -o "-c listen_addresses='localhost'" -w start
+        su-exec app psql -f /app.sql app
+        su-exec app pg_ctl -m fast -w stop
+    fi
+else
+    find /data -type d -exec chmod 700 {} \;
+    find /data -type f -exec chmod 600 {} \;
+fi
